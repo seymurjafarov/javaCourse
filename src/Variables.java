c2b3377afdfd1d29bb4d632d4f39b3a364fd282b
seymurjafarov <jafar.seymur@gmail.com>
2026-02-14 19:49:32 +0400

    variable clasina deyiklik edildi

--- src/Variables.java
+++ src/Variables.java
@@ -1,6 +1,8 @@
 public class Variables {
     public static void main(String[] args) {
         int myInt = 371;
+
+        System.out.println(myInt);
         long myLong = 613459l;
         double myDouble = 2.81;
         float myFloat = 45.912F;
