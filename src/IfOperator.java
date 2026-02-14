b9a46bea7f6874e3bbb2e1fa968a1290a0c6e7c7
seymurjafarov <jafar.seymur@gmail.com>
2026-02-14 19:47:38 +0400

    if klasi elave edildi

---
+++ src/IfOperator.java
@@ -0,0 +1,9 @@
+public class IfOperator {
+    public static void main(String[] args) {
+        int value = 14;
+
+        if(value<18){
+            System.out.println("18 yashdan kicikler satilmir");
+        }
+    }
+}
