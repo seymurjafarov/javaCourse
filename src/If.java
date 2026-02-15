fc2eeec6bd6069aa638bc81852fe2193457289cd
seymurjafarov <jafar.seymur@gmail.com>
2026-02-15 22:58:43 +0400

    If klasi elave edildi

--- src/If.java
+++ src/If.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class If {
     public static void main(String[] args) {
-        int value = 14;
 
-        if(value<18){
-            System.out.println("18 yashdan kicikler satilmir");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int value = sc.nextInt();
+
+        if (value >18) {
+            System.out.println("18 yashdan kiciklere icaze yoxdur");
+        } else if (value > 50) {
+            System.out.println("50 yashdan yuxari da icaze yoxdur");
+        } else {
+            System.out.println("Buyurun");
         }
+
+        main(args);
     }
+
 }
