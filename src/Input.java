47bee938018d823e4ec823cc4f3e6856e70bacc0
seymurjafarov <jafar.seymur@gmail.com>
2026-02-15 22:59:11 +0400

    Scanner ugurla tetbiq olundu

---
+++ src/Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter number");
+        int a = sc.nextInt();
+        System.out.println("please enter number");
+        int b = sc.nextInt();
+        System.out.println(a + b);
+
+    }
+}
